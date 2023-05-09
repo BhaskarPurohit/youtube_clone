7d81b58d90189383a8b94f9614a042cedf1d76b0
Bhaskar Purohit <bhaskarpurohit22@gmail.com>
2023-05-09 17:10:14 +0530

    9th may 02

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { AppContext } from './context/contextApi'
 import  Header  from './components/Header'
 import  Feed  from './components/Feed'
 import  LeftNav  from './components/LeftNav'
-// import  LeftNavMenuItem  from './components/LeftNavMenuItem'
+import  LeftNavMenuItem  from './components/LeftNavMenuItem'
 import  {SearchResult}  from './components/SearchResult'
 import  {VideoDetails}  from './components/VideoDetails'
 
@@ -22,13 +22,13 @@ const App = () => {
       <AppContext>
         <BrowserRouter>
           <div className="flex flex-col h-full">
-            <Header>
+            <Header/>
               <Routes>
                 <Route path='/'exact element={<Feed/>}/>
                 <Route path='/searchResult/:searchQuery' element={<SearchResult/>}/>
                 <Route path='/video/:id' element={<VideoDetails/>}/>
               </Routes>
-            </Header>
+            
           </div>
         </BrowserRouter>
 
