0cf83df701b1139143fb74957b08d3ec8f4b52b5
Bhaskar Purohit <bhaskarpurohit22@gmail.com>
2023-05-11 23:25:19 +0530

    11th may

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -7,6 +7,7 @@ import {VideoCard} from "./VideoCard"
 
 const Feed = () => {
   const {loading, searchResults} = useContext(Context)
+  
   useEffect(()=>{
     document.getElementById("root").classList.remove("custom-h")
   },[])
@@ -15,8 +16,8 @@ const Feed = () => {
       <LeftNav/>
       <div className="grow w-[calc(100%-240px)] h-full overlay-y-auto bg-black">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 p-5">
-          {!loading && searchResults.map((item)=>{
-            if(item.type !== 'video') return false
+          {!loading && searchResults && searchResults?.map((item)=>{
+            if(item?.type !== 'video') return false
             return(
               <VideoCard
               key={item?.video?.videoId}
