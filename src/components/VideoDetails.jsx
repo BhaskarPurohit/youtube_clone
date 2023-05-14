8169b6e530d96c110e4fa358b8eaf6a697dd26f0
Bhaskar Purohit <bhaskarpurohit22@gmail.com>
2023-05-14 23:29:04 +0530

    14 may

--- src/components/VideoDetails.jsx
+++ src/components/VideoDetails.jsx
@@ -1,4 +1,4 @@
-import {React, useState, useContext} from 'react'
+import {React, useState, useContext, useEffect} from 'react'
 import { useParams } from 'react-router-dom'
 import ReactPlayer from 'react-player'
 import {BsFillCheckCircleFill} from 'react-icons/bs'
@@ -14,9 +14,45 @@ const VideoDetails = () => {
   const [relatedVideos, setRelatedVideos] = useState()
   const {id} = useParams();
   const {setLoading} = useContext(Context)
-  return (
-    <div className='flex'></div>
+  
+  useEffect(()=>{
+    document.getElementById("root").classList.add("custom-h")
+    fetchVideoDetails()
+    fetchRelatedVideos()
+
+  },[id])
+
+  const fetchVideoDetails = ()=>{
+    setLoading(true)
+    fetchDataFromApi(`video/edtails/?id=${id}`).then((res)=>{
+      console.log(res);
+      setVideo(res)
+      setLoading(false)
+    })
+  }
+
+  const fetchRelatedVideos = ()=>{
+    setLoading(true)
+    fetchDataFromApi(`video/related-contents/?id=${id}`).then((res)=>{
+      console.log(res);
+      setRelatedVideos(res)
+      setLoading(false)
+    })
+  }
+
+  return(
+    <div className="flex justify-center flex-row h-[calc(100% - 50px)] bg-black">
+      <div className="w-full max-w-[1280px] flex flex-col lg:flex-row">
+        <div className="flex flex-col lg:w-[calc(100%-350px)] xl:w-[calc(100%-400px)] px-4 py-3 lg:py-6 overflow-y-auto">
+          <div className="h-[200px] md:h-[400px] lg:h-[400px] xl:h-[550px] ml-[16px] lg:ml-0 mr-[-16px] lg:mr-0">
+            <ReactPlayer/>
+          </div>
+        </div>
+      </div>
+    </div>
   )
+
+  
 }
 
 export default VideoDetails
