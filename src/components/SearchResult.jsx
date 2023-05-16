19218f1b0ad3c42a58e1610c0b2f27f434326973
Bhaskar Purohit <bhaskarpurohit22@gmail.com>
2023-05-16 15:44:34 +0530

    16.2

--- src/components/SearchResult.jsx
+++ src/components/SearchResult.jsx
@@ -13,7 +13,7 @@ const SearchResult = () => {
   useEffect(()=>{
     document.getElementById("root").classList.remove("custom-h")
     fetchSearchResults()
-    
+
     
 
   },[searchQuery])
@@ -27,7 +27,20 @@ const SearchResult = () => {
     })
   }
   return (
-    <div>SearchResult</div>
+    <div className='flex flex-row h-[calc(100%-56px)]'>
+      <LeftNav/>
+      <div className="grow w-[calc(100%-240px)] h-full overflow-y-auto bg-black">
+        <div className="grid grid-cols-1 gap-2 p-5">
+          {result?.map((item)=>{
+            if(item.type !== 'video') return false
+            let video = item.video
+            return(
+              <SearchResultVideoCard key={video?.videoId} video={video}/>
+            )
+          })}
+        </div>
+      </div>
+    </div>
   )
 }
 
